3d1ffa9c8399fe3acb58b8811461e4456f5f4f83
proudzhu <proudzhu.fdu@gmail.com>
2016-07-28 21:55:58 +0800

    finish observer pattern

--- c++/ch02/WeatherStation.cpp
+++ c++/ch02/WeatherStation.cpp
@@ -108,13 +108,165 @@ class CurrentConditionsDisplay :
     std::shared_ptr<Subject> weatherData_;
 };
 
+class StatisticsDisplay :
+  public std::enable_shared_from_this<StatisticsDisplay>,
+  public Observer,
+  public DisplayElement
+{
+  public:
+    StatisticsDisplay(std::shared_ptr<Subject> weatherData)
+      : weatherData_(weatherData)
+    { }
+
+    ~StatisticsDisplay()
+    { }
+
+    void registerDisplay()
+    {
+      weatherData_->registerObserver(shared_from_this());
+    }
+
+    void update(float temperature, float humidity, float pressure)
+    {
+      if (temperature > maxTemp_)
+        maxTemp_ = temperature;
+
+      if (temperature < minTemp_)
+        minTemp_ = temperature;
+
+      tempSum_ += temperature;
+      numReadings++;
+
+      display();
+    }
+
+    void display()
+    {
+      std::cout << "Avg/Max/Min temperature = "
+                << (tempSum_ / numReadings)
+                << "/"
+                << maxTemp_
+                << "/"
+                << minTemp_
+                << std::endl;
+    }
+
+  private:
+    float maxTemp_;
+    float minTemp_;
+    float tempSum_;
+    int numReadings;
+    std::shared_ptr<Subject> weatherData_;
+};
+
+class ForecastDisplay :
+  public std::enable_shared_from_this<ForecastDisplay>,
+  public Observer,
+  public DisplayElement
+{
+  public:
+    ForecastDisplay(std::shared_ptr<Subject> weatherData)
+      : weatherData_(weatherData)
+    { }
+
+    ~ForecastDisplay()
+    { }
+
+    void registerDisplay()
+    {
+      weatherData_->registerObserver(shared_from_this());
+    }
+
+    void update(float temperature, float humidity, float pressure)
+    {
+      lastPressure_ = currentPressure_;
+      currentPressure_ = pressure;
+
+      display();
+    }
+
+    void display()
+    {
+      std::cout << "Forecast: ";
+      if (currentPressure_ > lastPressure_)
+        std::cout << "Improving weather on the way!";
+      else if (currentPressure_ == lastPressure_)
+        std::cout << "More of the same";
+      else
+        std::cout << "Watch out for cooler, rainy weather";
+      std::cout << std::endl;
+    }
+
+  private:
+    float currentPressure_;
+    float lastPressure_;
+    std::shared_ptr<Subject> weatherData_;
+};
+
+class HeatIndexDisplay :
+  public std::enable_shared_from_this<HeatIndexDisplay>,
+  public Observer,
+  public DisplayElement
+{
+  public:
+    HeatIndexDisplay(std::shared_ptr<Subject> weatherData)
+      : weatherData_(weatherData)
+    { }
+
+    ~HeatIndexDisplay()
+    { }
+
+    void registerDisplay()
+    {
+      weatherData_->registerObserver(shared_from_this());
+    }
+
+    void update(float temperature, float humidity, float pressure)
+    {
+      heatIndex_ = computeHeatIndex(temperature, humidity);
+
+      display();
+    }
+
+    void display()
+    {
+      std::cout << "Heat index is " << heatIndex_ <<  std::endl;
+    }
+
+  private:
+    float heatIndex_;
+    std::shared_ptr<Subject> weatherData_;
+
+    float computeHeatIndex(float t, float rh) {
+      float index = (float)((16.923 + (0.185212 * t) + (5.37941 * rh) - (0.100254 * t * rh) +
+            (0.00941695 * (t * t)) + (0.00728898 * (rh * rh)) +
+            (0.000345372 * (t * t * rh)) - (0.000814971 * (t * rh * rh)) +
+            (0.0000102102 * (t * t * rh * rh)) - (0.000038646 * (t * t * t)) + (0.0000291583 *
+            (rh * rh * rh)) + (0.00000142721 * (t * t * t * rh)) +
+            (0.000000197483 * (t * rh * rh * rh)) - (0.0000000218429 * (t * t * t * rh * rh)) +
+            0.000000000843296 * (t * t * rh * rh * rh)) -
+            (0.0000000000481975 * (t * t * t * rh * rh * rh)));
+      return index;
+    }
+};
+
+
 int main(int argc, char **argv)
 {
   std::shared_ptr<WeatherData> weatherData = std::make_shared<WeatherData>();
   std::shared_ptr<CurrentConditionsDisplay> currentDisplay =
     std::make_shared<CurrentConditionsDisplay>(weatherData);
+  std::shared_ptr<StatisticsDisplay> statisticsDisplay =
+    std::make_shared<StatisticsDisplay>(weatherData);
+  std::shared_ptr<ForecastDisplay> forecastDisplay =
+    std::make_shared<ForecastDisplay>(weatherData);
+  std::shared_ptr<HeatIndexDisplay> heatIndexDisplay =
+    std::make_shared<HeatIndexDisplay>(weatherData);
 
   currentDisplay->registerDisplay();
+  statisticsDisplay->registerDisplay();
+  forecastDisplay->registerDisplay();
+  heatIndexDisplay->registerDisplay();
 
   weatherData->setMeasurements(80, 65, 30.4f);
   weatherData->setMeasurements(82, 70, 29.2f);
---
+++ java/ch02/WeatherData/src/StatisticsDisplay.java
@@ -0,0 +1,33 @@
+/**
+ * Created by proudzhu on 7/28/16.
+ */
+public class StatisticsDisplay implements Observer, DisplayElement {
+    private float maxTemp = 0.0f;
+    private float minTemp = 200;
+    private float tempSum = 0.0f;
+    private int numReadings;
+    private Subject weatherData;
+
+    public StatisticsDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    public void update(float temperature, float humidity, float pressure) {
+        if (temperature > maxTemp)
+            maxTemp = temperature;
+
+        if (temperature < minTemp)
+            minTemp = temperature;
+
+        tempSum += temperature;
+        numReadings++;
+
+        display();
+    }
+
+    public void display() {
+        System.out.println("Avg/Max/Min temperature = " + (tempSum / numReadings)
+            + "/" + maxTemp + "/" + minTemp);
+    }
+}
